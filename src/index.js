b962eb72cb51075350368bc8beab5b163173d33e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-08 15:41:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('Module 1.2');
+
+// ==== break ====
+
+/*
+for (let i = 0; i < 100; i += 1){
+  console.log(i);
+  
+  if (i === 5) {
+    console.log(i);
+    break;
+  }
+}
+*/
+
+// ==== continue ====
+
+/*
+for (let i = 0; i < 100; i += 1) {
+  if (i % 5 !== 0) {
+    continue;
+  }
+
+  console.log(i);
+}
+*/
