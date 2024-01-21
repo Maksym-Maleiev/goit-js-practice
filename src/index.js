4c73ab7eaf211e3dfb5fc20d0a54b2da6a29e96f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-21 19:49:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-const x = 5;
+console.log('module 02.2');
 
-console.log('Hello');
-console.log('program:', x);
+// ==== arrow function ====
+const x = 5;
+let y = 6;
+
+function fn1(x) {
+  console.log('fn1:', x);
+
+  return 7;
+}
+
+const fn3 = function (x) {
+  console.log('fn3:', x);
+
+  return 8;
+};
+
+const fn2 = x => {
+  console.log('fn2:', x);
+
+  return 9;
+};
+
+fn1(10);
+fn2(20);
+fn3(30);
+
+console.log('x:', x);
+console.log('y:', y); // 6
