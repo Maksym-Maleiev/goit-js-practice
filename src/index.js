04a07bc4a652479457584b1cd7a7d636ad0d2744
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-22 11:20:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,67 @@
 console.log('module 02.2');
 
 // ==== arrow function ====
-const x = 5;
-let y = 6;
+// const x = 5;
+// let y = 6;
 
-function fn1(x) {
-  console.log('fn1:', x);
+// function fn1(x) {
+//   console.log('fn1:', x);
 
-  return 7;
-}
+//   return 7;
+// }
 
-const fn3 = function (x) {
-  console.log('fn3:', x);
+// const fn3 = function (x) {
+//   console.log('fn3:', x);
 
-  return 8;
-};
+//   return 8;
+// };
 
-const fn2 = x => {
-  console.log('fn2:', x);
+// const fn2 = x => {
+//   console.log('fn2:', x);
 
-  return 9;
-};
+//   return 9;
+// };
 
-fn1(10);
-fn2(20);
-fn3(30);
+// fn1(10);
+// fn2(20);
+// fn3(30);
 
-console.log('x:', x);
-console.log('y:', y); // 6
+// console.log('x:', x);
+// console.log('y:', y); // 6
+
+// ==== ====
+
+// const a = 10;
+// const b = 1;
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log('result:', sum(1, 1));
+// console.log('result:', sum(10, 10));
+// console.log('result:', sum(10, 20));
+
+// ==== argument ====
+// function fn1() {
+//   console.log(Array.from(arguments));
+// }
+
+// const fn2 = (...args) => console.log(args);
+
+// fn1(1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 4);
+// fn2(1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 4);
+
+// ==== kahoot ====
+// const greet = function () {
+//   console.log('Wellcome');
+// };
+
+// greet();
+
+// ==== kahoot-1 ====
+// function foo() {
+//   console.log(arguments);
+// }
+
+// foo(5, 'Mango', true);
