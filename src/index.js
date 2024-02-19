3421b7e7eaaf94e164c7d2c3b416e00b954438af
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-19 15:23:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
-const x = 5;
+console.log('module_04.1');
 
-console.log('Hello');
-console.log('program:', x);
+//* ===== closure =====
+// const makeSheff = name => {
+//   const makeDish = dish => {
+//     console.log(`${name} готує ${dish}`);
+//   };
+
+//   return makeDish;
+// };
+
+// const sheff = makeSheff(`Max`); // makeDish
+
+// sheff(`котлети`); // - ось тут викликається внутрішня функція makeDish
+// sheff('пиріжок'); // - ось тут викликається внутрішня функція makeDish
+
+//* ===== closure 2 =====
+
+// const counter = (name, from = 1, step = 1) => {
+//   let i = from;
+
+//   return () => {
+//     console.log(`${name} - ${i}`);
+//     i += step;
+//   };
+// };
+
+// const counter1 = counter('counter1'); // function
+// const counter2 = counter('counter2', 10, 5); // function
+
+// counter1(); // 1
+// counter1(); // 2
+// counter1(); // 3
+// counter1(); // 4
+// counter1(); // 5
+
+// counter2(); // 10
+// counter2(); // 15
+// counter2();
+// counter2();
+// counter2();
+// counter2();
