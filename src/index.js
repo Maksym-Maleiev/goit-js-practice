5222feb104c23e1b5f73e28d0ede51c6f59472a5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-18 16:01:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('module_05.1');
+
+function sayHello() {
+  console.log(this);
+}
+
+const obj1 = {
+  name: 'Max',
+};
+
+const obj2 = {
+  name: 'Dasha',
+};
+
+const greet1 = sayHello.bind(obj1);
+setTimeout(greet1, 2000);
+
+const greet2 = sayHello.bind(obj2);
+setTimeout(greet2, 4000);
